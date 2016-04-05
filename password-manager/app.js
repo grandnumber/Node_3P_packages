c3bce6e2f435e068fb2f179792da2c438a54a6f2
grandnumber <thegrandnumber@gmail.com>
2016-04-05 12:19:54 -0700

    test basic persist module

--- password-manager/app.js
+++ password-manager/app.js
@@ -1,1 +1,9 @@
 console.log('starting password-manager');
+
+var storage = require('node-persist');
+storage.initSync();
+
+// storage.setItemSync('name', 'janet');
+
+var name = storage.getItemSync('name');
+console.log("retrieving name: " + name);

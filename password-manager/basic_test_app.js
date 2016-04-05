45f6f44ccc787720a7a27098909bcda2b4e747f8
grandnumber <thegrandnumber@gmail.com>
2016-04-05 15:46:41 -0700

    add yargv for create/retrieve accounts

--- password-manager/basic_test_app.js
+++ password-manager/basic_test_app.js
@@ -18,3 +18,51 @@
 // // save using setItemSync
 //
 // console.log(accounts);
+console.log('starting password-manager');
+
+var storage = require('node-persist');
+storage.initSync();
+
+// CREATE
+// --name
+// --username
+// --password
+
+// getItemSync
+
+// account.name Facebook
+// account.username User12!
+// account.password Password123!
+
+function createAccount (account){
+  if (typeof accounts === 'undefined') {
+    accounts = [];
+  }
+  accounts.push(account);
+  storage.setItemSync('accounts', accounts);
+
+  return account;
+}
+
+function getAccount (accountName){
+  var accounts = storage.getItemSync('accounts');
+	var matchedAccount;
+
+	accounts.forEach(function (account) {
+		if (account.name === accountName) {
+			matchedAccount = account;
+		}
+	});
+
+	return matchedAccount;
+}
+var baseAccount = {
+  name: 'Facebook',
+  username: 'User12!',
+  password: 'Password123'
+}
+
+// createAccount(baseAccount);
+
+var facebookAccount = getAccount('Facebook');
+console.log(facebookAccount);

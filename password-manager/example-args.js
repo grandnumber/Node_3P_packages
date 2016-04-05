96792bcc81e23ba5366c4eb94a84135a35f5e5da
grandnumber <thegrandnumber@gmail.com>
2016-04-05 15:10:07 -0700

    add basic test

--- password-manager/example-args.js
+++ password-manager/example-args.js
@@ -1,7 +1,13 @@
 var argv = require('yargs').argv;
-
+var command = argv._[0]
 console.log(argv);
 
-if (argv._[0] === 'hello') {
-	console.log('Hello World !');
+if (command === 'hello' && typeof argv.name !== 'undefined' && typeof argv.lastname !== 'undefined') {
+	console.log('Hello ' + argv.name + ' ' + argv.lastname + '!');
+}  else  {
+	console.log('Hello World & Hello '  + argv.name);
 }
+
+
+// ABILITY TO CREATE NAMED ARGS IN TERMINAL
+// $ node example-args.js hello --name Janet
